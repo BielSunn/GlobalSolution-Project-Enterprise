a41a065107c1e963ba3ef823dbec6b02bc48b71c
Gabriel Sun <bielsun23@gmail.com>
2022-11-03 00:20:58 -0300

    add Banco de dados e configurado

---
+++ GlobalSolution.Project.Web/Persistencia/GlobalSolutionContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GlobalSolution.Project.Web.Persistencia
+{
+    public class GlobalSolutionContext : DbContext
+    {
+
+        public GlobalSolutionContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
--- GlobalSolution.Project.Web/Program.cs
+++ GlobalSolution.Project.Web/Program.cs
@@ -1,9 +1,30 @@
+using GlobalSolution.Project.Web.Persistencia;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Obter a string de conexão do arquivo appsettings.json
+var sql = builder.Configuration.GetConnectionString("conexao");
+
+//Configurar o serviço de DbContext para a injeção de dependencia
+builder.Services.AddDbContext<GlobalSolutionContext>(op => op.UseSqlServer(sql));
+
 var app = builder.Build();
+
+//Configurar Data padrão PT-BR
+var supportedCultures = new[] { new CultureInfo("pt-BR") };
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(culture: "pt-BR", uiCulture: "pt-BR"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+});
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
